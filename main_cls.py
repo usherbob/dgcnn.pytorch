6791a479af2ea218ea07176a156b50cc11fcd434
Bob Wang <14291254@gmail.com>
2020-10-10 22:12:48 +0800

    use node1_static to calc loss in main_cls

--- main_cls.py
+++ main_cls.py
@@ -202,9 +202,10 @@ def test(args, io):
         test_true.append(label.cpu().numpy())
         test_pred.append(preds.detach().cpu().numpy())
         if args.visu and count % 5 == 0:
-            for i in range(node.shape[0]):
-                np.save('/root/ckpt/cls/%s/visu/node_%04d.npy' % (args.exp_name, count*args.test_batch_size+i), node[i, :, :].detach().cpu().numpy())
+            for i in range(data.shape[0]):
                 np.save('/root/ckpt/cls/%s/visu/data_%04d.npy' % (args.exp_name, count*args.test_batch_size+i), data[i, :, :].detach().cpu().numpy())
+                np.save('/root/ckpt/cls/%s/visu/node1_%04d.npy' % (args.exp_name, count*args.test_batch_size+i), node1[i, :, :].detach().cpu().numpy())
+                np.save('/root/ckpt/cls/%s/visu/node2_%04d.npy' % (args.exp_name, count*args.test_batch_size+i), node2[i, :, :].detach().cpu().numpy())
     test_true = np.concatenate(test_true)
     test_pred = np.concatenate(test_pred)
     test_acc = metrics.accuracy_score(test_true, test_pred)
