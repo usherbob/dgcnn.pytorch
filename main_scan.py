0069cf4a8a9a3de4bd1fc68f9ab29747a36e23c4
Bob Wang <14291254@gmail.com>
2020-10-19 17:28:43 +0800

    two level model for scan object data

--- main_scan.py
+++ main_scan.py
@@ -45,9 +45,9 @@ def _init_():
     os.system('cp data.py ' + ckpt_dir + '/' + args.exp_name + '/' + 'data.py.backup')
 
 def train(args, io):
-    train_loader = DataLoader(ScanObject(h5_filename='train', num_points=args.num_points), num_workers=8,
+    train_loader = DataLoader(ScanObject(h5_filename='/opt/data/private/data/ScanObjectNN/main_split/training_objectdataset.h5', num_points=args.num_points), num_workers=8,
                               batch_size=args.batch_size, shuffle=True, drop_last=True)
-    test_loader = DataLoader(ScanObject(h5_filename='test', num_points=args.num_points), num_workers=8,
+    test_loader = DataLoader(ScanObject(h5_filename='/opt/data/private/data/ScanObjectNN/main_split/test_objectdataset.h5', num_points=args.num_points), num_workers=8,
                              batch_size=args.test_batch_size, shuffle=True, drop_last=False)
 
     device = torch.device("cuda" if args.cuda else "cpu")
@@ -186,8 +186,8 @@ def train(args, io):
 
 
 def test(args, io):
-    test_loader = DataLoader(ScanObject(h5_filename='test', num_points=args.num_points),
-                             batch_size=args.test_batch_size, shuffle=False, drop_last=False)
+    test_loader = DataLoader(ScanObject(h5_filename='/opt/data/private/data/ScanObjectNN/main_split/training_objectdataset.h5',
+                                        num_points=args.num_points), batch_size=args.test_batch_size, shuffle=False, drop_last=False)
 
     device = torch.device("cuda" if args.cuda else "cpu")
 
