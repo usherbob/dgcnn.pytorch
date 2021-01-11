74e63187abefc8a679a659703242ef24abc97267
Bob Wang <14291254@gmail.com>
2021-01-11 16:00:37 +0800

    version 3 for relu after addition

--- model.py
+++ model.py
@@ -195,7 +195,7 @@ class PointNet(nn.Module):
 
         x2 = F.relu(self.bn3(self.conv3(x_p1)))
         x2 = F.relu(self.bn4(self.conv4(x2)))                                            #(batch_size, 128, num_points//4)
-        x2 = x2 + x_p1
+        x2 = F.relu(x2 + x_p1)
         x_t2 = F.relu(self.bn4_m(self.conv4_m(x2)))                                     #(batch_size, 512, num_points//4)
 
         node_features, values, idx, ret2, node2_static, node2 = self.pool2(node1_static, x2)
@@ -204,7 +204,7 @@ class PointNet(nn.Module):
 
         x3 = F.relu(self.bn5(self.conv5(x_p2)))
         x3 = F.relu(self.bn6(self.conv6(x3)))                                            #(batch_size, 256, num_points//16)
-        x3 = x3 + x_p2
+        x3 = F.relu(x3 + x_p2)
         x_t3 = F.relu(self.bn6_m(self.conv6_m(x3)))                                     #(batch_size, 512, num_points//16)
 
         node_features, values, idx, ret3, node3_static, node3 = self.pool3(node2_static, x3)
@@ -213,7 +213,7 @@ class PointNet(nn.Module):
 
         x4 = F.relu(self.bn7(self.conv7(x_p3)))
         x4 = F.relu(self.bn8(self.conv8(x4)))
-        x_t4 = x4 + x_p3
+        x_t4 = F.relu(x4 + x_p3)
 
         x = torch.cat((F.adaptive_max_pool1d(x_t1, 1).squeeze(), F.adaptive_max_pool1d(x_t2, 1).squeeze(),
                        F.adaptive_max_pool1d(x_t3, 1).squeeze(), F.adaptive_max_pool1d(x_t4, 1).squeeze(),), dim=1)
