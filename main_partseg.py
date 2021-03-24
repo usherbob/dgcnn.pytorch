474a1f14571378b4691708abadbf20ac82c99e40
usherbob <14291254@bjtu.edu.cn>
2021-03-24 22:13:32 +0800

    利用Rand4分支中的partseg框架，实现MIPooling对应的部件分割模型

--- main_partseg.py
+++ main_partseg.py
@@ -353,7 +353,7 @@ if __name__ == "__main__":
                         help='Turn on residual connection')
     parser.add_argument('--cd_weights', type=float, default=0.0, metavar='LR',
                         help='weights of cd_loss')
-    parser.add_argument('--pool', action='store_true',
+    parser.add_argument('--pool', action='store_false',
                         help='whether use pooling module')
     args = parser.parse_args()
 
--- part_job.sh
+++ part_job.sh
@@ -1,3 +1,5 @@
-CUDA_VISIBLE_DEVICES=6 python main_partseg.py --base_dir /data/sunzhiyi/pc --exp_name res0317 --res True
-wait
-CUDA_VISIBLE_DEVICES=6 python main_partseg.py --base_dir /data/sunzhiyi/pc --exp_name vanilla0317
+#!/bin/sh
+for i in '111' '222'
+do
+echo $i
+done
