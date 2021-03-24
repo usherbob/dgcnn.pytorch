b2cedea167a74d6585492fee5b311c2f67678724
usherbob <14291254@bjtu.edu.cn>
2021-03-24 16:12:10 +0800

    利用Rand4分支中的partseg框架，实现MIPooling对应的部件分割模型

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ import torch
 import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
-from util import knn, get_graph_feature, unpool, aggregate, Discriminator, EdgeConv, IndexSelect
+from util import knn, get_graph_feature, unpool, aggregate, Discriminator, EdgeConv, IndexSelect, MIPool
 
 
 class PointNet(nn.Module):
@@ -547,9 +547,9 @@ class DGCNN_partseg(nn.Module):
         self.k = args.k
         self.transform_net = Transform_Net(args)
 
-        self.pool1 = RandPool(self.args.num_points//4,  self.k, 64)
-        self.pool2 = RandPool(self.args.num_points//16, self.k, 64)
-        self.pool3 = RandPool(self.args.num_points//64, self.k, 64)
+        self.pool1 = MIPool(self.args.num_points//4,  self.k, 64)
+        self.pool2 = MIPool(self.args.num_points//16, self.k, 64)
+        self.pool3 = MIPool(self.args.num_points//64, self.k, 64)
 
         self.ec0 = EdgeConv(num_neighs=self.k,    dims=[3, 64, 64])
         self.pn0 = MLP([64, 1024])
