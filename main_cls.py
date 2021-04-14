4b293a4936bb7180a47142e93dd602dc8efdee70
usherbob <14291254@bjtu.edu.cn>
2021-04-14 13:49:43 +0800

    修正可视化中的一些命名问题

--- main_cls.py
+++ main_cls.py
@@ -214,10 +214,10 @@ def test(args, io):
         preds = logits.max(dim=1)[1]
         test_true.append(label.cpu().numpy())
         test_pred.append(preds.detach().cpu().numpy())
-        if args.visu and count % 5 == 0:
+        if args.visu:
             for i in range(data.shape[0]):
-                np.save(BASE_DIR + '/ckpt/cls/%s/visu/node0_%04d.npy' % (args.exp_name, count*args.test_batch_size+i), data[i, :, :].detach().cpu().numpy())
-                np.save(BASE_DIR + '/ckpt/cls/%s/visu/node1_%04d.npy' % (args.exp_name, count*args.test_batch_size+i), node1_static[i, :, :].detach().cpu().numpy())
+                np.save(BASE_DIR + '/ckpt/cls/%s/visu/node0_%02d_%04d.npy' % (args.exp_name, lable[i], count*args.test_batch_size+i), data[i, :, :].detach().cpu().numpy())
+                np.save(BASE_DIR + '/ckpt/cls/%s/visu/node1_%02d_%04d.npy' % (args.exp_name, label[i], count*args.test_batch_size+i), node1_static[i, :, :].detach().cpu().numpy())
     test_true = np.concatenate(test_true)
     test_pred = np.concatenate(test_pred)
     test_acc = metrics.accuracy_score(test_true, test_pred)
