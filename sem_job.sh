8bd37e34db333a5a7f4aa73694c881e688f97625
usherbob <14291254@bjtu.edu.cn>
2021-06-19 07:14:53 +0000

    增加对于预训练模型inference的支持

--- sem_job.sh
+++ sem_job.sh
@@ -1,11 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python main_semseg.py --base_dir /data/sunzhiyi/pc --test_area 6 --exp_name res0317_6
+CUDA_VISIBLE_DEVICES=1 python main_semseg.py --base_dir /data4/jiahuawang --test_area 6 --eval True --exp_name randSOTA.eval --model_root /data4/jiahuawang/ckpt/semseg/randSOTA/
 wait
-CUDA_VISIBLE_DEVICES=0 python main_semseg.py --base_dir /data/sunzhiyi/pc --test_area 5 --exp_name res0317_5
-wait
-CUDA_VISIBLE_DEVICES=0 python main_semseg.py --base_dir /data/sunzhiyi/pc --test_area 4 --exp_name res0317_4
-wait
-CUDA_VISIBLE_DEVICES=0 python main_semseg.py --base_dir /data/sunzhiyi/pc --test_area 3 --exp_name res0317_3
-wait
-CUDA_VISIBLE_DEVICES=0 python main_semseg.py --base_dir /data/sunzhiyi/pc --test_area 2 --exp_name res0317_2
-wait
-CUDA_VISIBLE_DEVICES=0 python main_semseg.py --base_dir /data/sunzhiyi/pc --test_area 1 --exp_name res0317_1
